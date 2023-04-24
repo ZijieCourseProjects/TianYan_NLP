237519c8eb56a7ad423332ad9c1cb8e3b5e84cff
Ricardo050212 <1310788243@qq.com>
2023-04-24 09:16:54 +0800

    测试

--- tests/test_TopicModelingandDimensionReduction.py
+++ tests/test_TopicModelingandDimensionReduction.py
@@ -21,38 +21,27 @@ class Test(TestCase):
         for topic in lda_model.print_topics():
             print(topic)
 
-
-class Test(TestCase):
     def test_fit_lsa(self):
         num_topics = 2
         lsa_model = tm.fit_lsa(bag=bow_corpus, numTopics=num_topics, counts=None)
 
-
-class Test(TestCase):
     def test_resume_lda_model(self):
         num_topics = 4
         lda_model = tm.fit_lda(bag=bow_corpus, numTopics=num_topics, counts=None, Solver='cvb0')
         tm.resume_lda_model(ldamdl=lda_model, bag=bow_corpus, counts=None, LogLikelihoodTolerance=0.00001)
 
-
-
-class Test(TestCase):
     def test_logprob_and_gdns_lda(self):
         num_topics = 2
         lda_model = tm.fit_lda(bag=bow_corpus, numTopics=num_topics, counts=None, Verbose=0)
         logprob = tm.logprob_and_gdns_lda(ldamdl=lda_model, documents=None, bag=bow_corpus, counts=None)
         print(logprob)
 
-
-class Test(TestCase):
     def test_predict(self):
         num_topics = 2
         lda_model = tm.fit_lda(bag=bow_corpus, numTopics=num_topics, counts=None)
         topicIdx = tm.predict(bag=bow_corpus, documents=None, counts=None, ldamdl=lda_model)
         print(topicIdx)
 
-
-class Test(TestCase):
     def test_transform_to_lowdimension(self):
         num_topics = 2
         lda_model = tm.fit_lda(bag=bow_corpus, numTopics=num_topics, counts=None)

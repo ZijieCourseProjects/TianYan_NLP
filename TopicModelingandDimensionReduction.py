58fa7929cf104a76b64bd9a5a437e6f2e409590b
Ricardo050212 <1310788243@qq.com>
2023-04-27 22:12:31 +0800

    函数实现完成

--- TopicModelingandDimensionReduction.py
+++ TopicModelingandDimensionReduction.py
@@ -1,4 +1,11 @@
-def fit_lda(bag, numTopics, counts,
+import gensim.models
+from gensim import corpora, models
+from sklearn.decomposition import TruncatedSVD
+from gensim.matutils import Sparse2Corpus
+from scipy.sparse import csr_matrix
+import numpy as np
+
+def fit_lda(numTopics, counts=None, bag=None,
             Solver='cgs', LogLikelihoodTolerance=0.0001, FitTopicProbabilities=True, FitTopicConcentration=None,
             InitialTopicConcentration=4, TopicOrder='initial-fit-probability', WordConcentration=1, DocumentsIn='rows',
             IterationLimit=100,
@@ -40,26 +47,68 @@ def fit_lda(bag, numTopics, counts,
     :param Verbose:Verbosity level , values: 1 | 0
     :return:Output LDA model, returned as an ldaModel object.
     """
+    if counts is None and bag is None:
+        raise TypeError('no corpus is given!')
+    elif counts is not None and bag is not None:
+        raise TypeError('two corpus are given!')
+    # input is bagofwords
+    elif counts is None:
+        if TopicOrder == 'initial-fit-probability':
+            lda = gensim.models.LdaModel(corpus=bag, num_topics=numTopics, decay=LearnRateDecay, per_word_topics=True)
+        else:
+            lda = gensim.models.LdaModel(corpus=bag, num_topics=numTopics, decay=LearnRateDecay)
+    # input is matrix of word frequncy
+    elif bag is None:
+        matrix = counts
+        # turn matrix into bow
+        corpus_of_matrix = [list(zip(range(len(row)), row)) for row in matrix]
+        # get vocabulary
+        id2word = corpora.Dictionary.from_corpus(corpus_of_matrix)
 
-    raise NotImplementedError
+        if TopicOrder == 'initial-fit-probability':
+            lda = gensim.models.LdaModel(corpus=corpus_of_matrix, id2word=id2word, num_topics=numTopics,
+                                         decay=LearnRateDecay, per_word_topics=True)
+        else:
+            lda = gensim.models.LdaModel(corpus=corpus_of_matrix, id2word=corpus_of_matrix, num_topics=numTopics,
+                                         decay=LearnRateDecay)
+
+    return lda
 
 
-def fit_lsa(bag, numTopics, counts, DocumentsIn='rows', FeatureStrengthExponent=2):
+def fit_lsa(numComponents, bag=None, counts=None, DocumentsIn='rows', FeatureStrengthExponent=2):
     """
 
     :param bag: Input bag-of-words or bag-of-n-grams model
-    :param numTopics: Number of topics e.g. 200
+    :param numComponents: Number of components e.g. 200
     :param counts: Frequency counts of words, specified as a matrix of nonnegative integers
     :param DocumentsIn:Orientation of documents in the word count matrix  values: 'rows' (default) | 'columns'
     :param FeatureStrengthExponent:Initial feature strength exponent value:2(default) | nonnegative scalar
     :return:Output LSA model, returned as an lsaModel object.
     """
+    if counts is None and bag is None:
+        raise TypeError('no corpus is given!')
+    elif counts is not None and bag is not None:
+        raise TypeError('two corpus are given!')
 
-    raise NotImplementedError
+    # input is bagofwords
+    elif counts is None:
+        lsa = models.LsiModel(corpus=bag,num_topics=numComponents)
+
+    # input is matrix of word frequncy
+    elif bag is None:
+        matrix = counts
+        matrix_sparse = csr_matrix(matrix)
+        # 将稀疏矩阵转换为词袋模型
+        corpus = Sparse2Corpus(matrix_sparse.transpose())
+
+        lsa = models.LsiModel(corpus=corpus, num_topics=2)
+
+    return lsa
 
 
-def resume_lda_model(ldamdl, bag, counts,
-                     LogLikelihoodTolerance=0.0001, FitTopicProbabilities=None, FitTopicConcentration=None, DocumentsIn='rows',
+def resume_lda_model(ldamdl:gensim.models.LdaModel, bag=None, counts=None,
+                     LogLikelihoodTolerance=0.0001, FitTopicProbabilities=None, FitTopicConcentration=None,
+                     DocumentsIn='rows',
                      IterationLimit=100,
                      DataPassLimit=1, MiniBatchLimit=None, MiniBatchSize=1000,
                      ValidationData=None, ValidationFrequency=None, Verbose=1):
@@ -94,10 +143,25 @@ def resume_lda_model(ldamdl, bag, counts,
     :return:Output LDA model, returned as an ldaModel object.
     """
 
-    raise NotImplementedError
+    if counts is None and bag is None:
+        raise TypeError('no corpus is given!')
+    elif counts is not None and bag is not None:
+        raise TypeError('two corpus are given!')
+    # input is bagofwords
+    elif counts is None:
+        ldamdl.update(corpus=bag)
+    # input is matrix of word frequncy
+    elif bag is None:
+        matrix = counts
+        # turn matrix into bow
+        corpus_of_matrix = [list(zip(range(len(row)), row)) for row in matrix]
+        # get vocabulary
+        ldamdl.update(corpus=corpus_of_matrix)
+
+    return ldamdl
 
 
-def logprob_and_gdns_lda(ldamdl, documents, bag, counts, DocumentsIn='rows', NumSamples = 1000):
+def logprob_and_gdns_lda(ldamdl:gensim.models.LdaModel, documents=None, bag=None, counts=None, DocumentsIn='rows', NumSamples=1000):
     """
 
     :param ldamdl: Input LDA model
@@ -110,11 +174,53 @@ def logprob_and_gdns_lda(ldamdl, documents, bag, counts, DocumentsIn='rows', Num
     :return: logProb: Log-probabilities of the documents under the LDA model , numeric vector.
              ppl: Perplexity of the documents calculated from the log-probabilities , positive scalar
     """
+    log_probabilitys = []
+    if bag is not None:
+        for doc_id in range(0, len(bag)):
+            doc_bow = bag[doc_id]
+            doc_topics = ldamdl.get_document_topics(doc_bow, minimum_probability=0)
+            doc_logprob = np.log([p for _, p in doc_topics])
+            log_probabilitys.append(doc_logprob)
 
-    raise NotImplementedError
+        ppl = -ldamdl.log_perplexity(chunk=bag)
+
+        return log_probabilitys, ppl
+
+    elif documents is not None:
+        dictionary = corpora.Dictionary()
+        for text in documents:
+            dictionary.add_documents([text.lower().split()])
+
+        corpus_bow = [dictionary.doc2bow(text.lower().split()) for text in documents]
+
+        for doc_id in range(0, len(corpus_bow)):
+            doc_bow = corpus_bow[doc_id]
+            doc_topics = ldamdl.get_document_topics(doc_bow, minimum_probability=0)
+            doc_logprob = np.log([p for _, p in doc_topics])
+            log_probabilitys.append(doc_logprob)
+
+        ppl = -ldamdl.log_perplexity(chunk=corpus_bow)
+        return log_probabilitys, ppl
+
+    elif counts is not None:
+        matrix = counts
+        matrix_sparse = csr_matrix(matrix)
+        # 将稀疏矩阵转换为词袋模型
+        corpus = Sparse2Corpus(matrix_sparse.transpose())
+        for doc_id in range(0, len(corpus)):
+            doc_bow = corpus[doc_id]
+            doc_topics = ldamdl.get_document_topics(doc_bow, minimum_probability=0)
+            doc_logprob = np.log([p for _, p in doc_topics])
+            log_probabilitys.append(doc_logprob)
+
+        ppl = -ldamdl.log_perplexity(chunk=corpus)
+        return log_probabilitys, ppl
+
+    else:
+        print("Please give the a corpus:document | bag-of-word | frequency matrix of word")
 
 
-def predict(ldamdl, documents, bag, counts, DocumentsIn='rows', IterationLimit=100, LogLikelihoodTolerance=0.0001):
+def predict(ldamdl, documents=None, bag=None, counts=None, DocumentsIn='rows', IterationLimit=100, LogLikelihoodTolerance=0.0001):
     """
 
     :param ldamdl: Input LDA model
@@ -129,11 +235,91 @@ def predict(ldamdl, documents, bag, counts, DocumentsIn='rows', IterationLimit=1
     :return: topicIdx: Predicted topic indices , vector of numeric indices.
              score: Predicted topic probabilities , matrix
     """
+    # 每篇文档最可能的主题的索引向量
+    top_topics_predict = []
+    # 主题出现在文档中的概率矩阵
+    topic_scores = []
+    if bag is not None:
+        for doc_id in range(0, len(bag)):
+            doc_bow = bag[doc_id]
+            doc_topics = ldamdl.get_document_topics(doc_bow, minimum_probability=0)
+            # 将得分写入列表
+            doc_score = []
+            for tu in doc_topics:
+                doc_score.append(tu[1])
+            topic_scores.append(doc_score)
+            # 计算得分最高的主题
+            top_topic = 0
+            probability = doc_topics[0][1]
+            for i in range(1, len(doc_topics)):
+                if doc_topics[i][1] > probability:
+                    top_topic = i
+                    probability = doc_topics[i][1]
 
-    raise NotImplementedError
+            top_topics_predict.append(top_topic)
+
+        topic_scores = np.array(topic_scores)
+        return top_topics_predict, topic_scores
+
+    elif documents is not None:
+        dictionary = corpora.Dictionary()
+        for text in documents:
+            dictionary.add_documents([text.lower().split()])
+
+        corpus_bow = [dictionary.doc2bow(text.lower().split()) for text in documents]
+
+        for doc_id in range(0, len(corpus_bow)):
+            doc_bow = corpus_bow[doc_id]
+            doc_topics = ldamdl.get_document_topics(doc_bow, minimum_probability=0)
+            # 将得分写入列表
+            doc_score = []
+            for tu in doc_topics:
+                doc_score.append(tu[1])
+            topic_scores.append(doc_score)
+            # 计算得分最高的主题
+            top_topic = 0
+            probability = doc_topics[0][1]
+            for i in range(1, len(doc_topics)):
+                if doc_topics[i][1] > probability:
+                    top_topic = i
+                    probability = doc_topics[i][1]
+
+            top_topics_predict.append(top_topic)
+
+        topic_scores = np.array(topic_scores)
+        return top_topics_predict, topic_scores
+
+    elif counts is not None:
+        matrix = counts
+        matrix_sparse = csr_matrix(matrix)
+        # 将稀疏矩阵转换为词袋模型
+        corpus = Sparse2Corpus(matrix_sparse.transpose())
+        for doc_id in range(0, len(corpus)):
+            doc_bow = corpus[doc_id]
+            doc_topics = ldamdl.get_document_topics(doc_bow, minimum_probability=0)
+            # 将得分写入列表
+            doc_score = []
+            for tu in doc_topics:
+                doc_score.append(tu[1])
+            topic_scores.append(doc_score)
+            # 计算得分最高的主题
+            top_topic = 0
+            probability = doc_topics[0][1]
+            for i in range(1, len(doc_topics)):
+                if doc_topics[i][1] > probability:
+                    top_topic = i
+                    probability = doc_topics[i][1]
+
+            top_topics_predict.append(top_topic)
+
+        topic_scores = np.array(topic_scores)
+        return top_topics_predict, topic_scores
+
+    else:
+        print("Please give the a corpus:document | bag-of-word | frequency matrix of word")
 
 
-def transform_to_lowdimension(lsamdl, ldamdl, documents, bag, counts,
+def transform_to_lowdimension(lsamdl=None, ldamdl=None, documents=None, bag=None, counts=None,
                               DocumentsIn='rows', IterationLimit=100, LogLikelihoodTolerance=0.0001):
     """
 
@@ -150,4 +336,110 @@ def transform_to_lowdimension(lsamdl, ldamdl, documents, bag, counts,
     :return: dscores: Output document scores , matrix of score vectors.
     """
 
-    raise NotImplementedError
+    dscore_matrix = []
+    # use lda model to transform
+    if ldamdl is not None:
+        # documents input
+        if documents is not None:
+            new_word_list = [doc.split() for doc in documents]
+            dictionary = corpora.Dictionary(new_word_list)
+            new_corpus = [dictionary.doc2bow(doc.split()) for doc in documents]
+
+            new_doc_topic_dist = ldamdl[new_corpus]
+
+            # 打印转换后的主题分布向量矩阵
+            for doc in new_doc_topic_dist:
+                dscore_of_doc = [tu[1] for tu in doc[0]]
+
+                dscore_matrix.append(dscore_of_doc)
+
+            dscore_matrix = np.array(dscore_matrix)
+
+            return dscore_matrix
+        # bag input
+        if bag is not None:
+            new_bag_topic_dist = ldamdl[bag]
+
+            for doc in new_bag_topic_dist:
+                dscore_of_doc = [tu[1] for tu in doc[0]]
+
+                dscore_matrix.append(dscore_of_doc)
+
+            dscore_matrix = np.array(dscore_matrix)
+
+            return dscore_matrix
+
+        # matrix input
+        if counts is not None:
+            matrix = counts
+            matrix_sparse = csr_matrix(matrix)
+            # 将稀疏矩阵转换为词袋模型
+            corpus = Sparse2Corpus(matrix_sparse.transpose())
+
+            new_count_topic_dist = ldamdl[corpus]
+
+            for doc in new_count_topic_dist:
+                dscore_of_doc = [tu[1] for tu in doc[0]]
+
+                dscore_matrix.append(dscore_of_doc)
+
+            dscore_matrix = np.array(dscore_matrix)
+
+            return dscore_matrix
+
+        else:
+            print("Please give the a corpus:document | bag-of-word | frequency matrix of word")
+
+    # use lsa model to transform
+    if lsamdl is not None:
+        # documents input
+        if documents is not None:
+            new_word_list = [doc.split() for doc in documents]
+            dictionary = corpora.Dictionary(new_word_list)
+            new_corpus = [dictionary.doc2bow(doc.split()) for doc in documents]
+
+            vectors = lsamdl[new_corpus]
+
+            dscore_matrix = np.zeros((len(new_corpus), lsamdl.num_topics))
+            i = 0
+            for vec in vectors:
+                for tu in vec:
+                    dscore_matrix[i, tu[0]] = tu[1]
+                i = i + 1
+
+            return dscore_matrix
+        # bag input
+        if bag is not None:
+            vectors = lsamdl[bag]
+
+            dscore_matrix = np.zeros((len(bag), lsamdl.num_topics))
+            i = 0
+            for vec in vectors:
+                for tu in vec:
+                    dscore_matrix[i, tu[0]] = tu[1]
+                i = i + 1
+
+            return dscore_matrix
+
+        # matrix input
+        if counts is not None:
+            matrix = counts
+            matrix_sparse = csr_matrix(matrix)
+            # 将稀疏矩阵转换为词袋模型
+            corpus = Sparse2Corpus(matrix_sparse.transpose())
+            vectors = lsamdl[corpus]
+
+            dscore_matrix = np.zeros((matrix.shape[0], lsamdl.num_topics))
+            i = 0
+            for vec in vectors:
+                for tu in vec:
+                    dscore_matrix[i, tu[0]] = tu[1]
+                i = i + 1
+
+            return dscore_matrix
+
+        else:
+            print("Please give the a corpus:document | bag-of-word | frequency matrix of word")
+
+
+
--- tests/test_TopicModelingandDimensionReduction.py
+++ tests/test_TopicModelingandDimensionReduction.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from gensim import corpora, models
+import numpy as np
 import TopicModelingandDimensionReduction as tm
 
 
@@ -12,43 +13,114 @@ dictionary = corpora.Dictionary(corpus)
 # Create bag-of-words representation of corpus
 bow_corpus = [dictionary.doc2bow(doc) for doc in corpus]
 
+# counts of frequency
+matrix = np.array([[1, 2, 0], [2, 1, 0], [0, 0, 2], [0, 0, 2]])
+
+document_fruit = ["apple banana grape orange", "peach orange kiwi apple", "grape banana peach apple"]
 
 class Test(TestCase):
     def test_fit_lda(self):
         num_topics = 2
-        lda_model = tm.fit_lda(bag=bow_corpus, numTopics=num_topics, counts=None)
+        lda_model = tm.fit_lda(bag=bow_corpus, numTopics=num_topics)
+        # Print topics and their top words
+        for topic in lda_model.print_topics():
+            print(topic)
+
+    def test_fit_lda_matrixinput(self):
+        num_topics = 2
+
+        lda_model = tm.fit_lda(counts=matrix, numTopics=num_topics)
         # Print topics and their top words
         for topic in lda_model.print_topics():
             print(topic)
 
     def test_fit_lsa(self):
-        num_topics = 2
-        lsa_model = tm.fit_lsa(bag=bow_corpus, numTopics=num_topics, counts=None)
+        numcomponents = 2
+        lsa_model = tm.fit_lsa(bag=bow_corpus, numComponents=numcomponents)
+        for topic in lsa_model.print_topics():
+            print(topic)
+
+    def test_fit_lsa_matrixinput(self):
+        numcomponents = 2
+        lsa_model = tm.fit_lsa(counts=matrix, numComponents=numcomponents)
+        for topic in lsa_model.print_topics():
+            print(topic)
 
     def test_resume_lda_model(self):
         num_topics = 4
         lda_model = tm.fit_lda(bag=bow_corpus, numTopics=num_topics, counts=None, Solver='cvb0')
-        tm.resume_lda_model(ldamdl=lda_model, bag=bow_corpus, counts=None, LogLikelihoodTolerance=0.00001)
+        tm.resume_lda_model(ldamdl=lda_model, bag=bow_corpus)
+        for topic in lda_model.print_topics():
+            print(topic)
+
+    def test_resume_lda_model_matrixinput(self):
+        num_topics = 4
+        lda_model = tm.fit_lda(bag=bow_corpus, numTopics=num_topics, counts=None, Solver='cvb0')
+        tm.resume_lda_model(ldamdl=lda_model,counts=matrix)
+        for topic in lda_model.print_topics():
+            print(topic)
 
     def test_logprob_and_gdns_lda(self):
         num_topics = 2
-        lda_model = tm.fit_lda(bag=bow_corpus, numTopics=num_topics, counts=None, Verbose=0)
-        logprob = tm.logprob_and_gdns_lda(ldamdl=lda_model, documents=None, bag=bow_corpus, counts=None)
+        lda_model = tm.fit_lda(bag=bow_corpus, numTopics=num_topics)
+        logprob, ppl = tm.logprob_and_gdns_lda(ldamdl=lda_model, bag=bow_corpus)
         print(logprob)
+        print(ppl)
+
+    def test_logprob_and_gdns_lda_document_input(self):
+        num_topics = 2
+        lda_model = tm.fit_lda(bag=bow_corpus, numTopics=num_topics)
+        logprob, ppl= tm.logprob_and_gdns_lda(ldamdl=lda_model, documents=document_fruit)
+        print(logprob)
+        print(ppl)
+
+    def test_logprob_and_gdns_lda_matrix_input(self):
+        num_topics = 2
+        lda_model = tm.fit_lda(bag=bow_corpus, numTopics=num_topics)
+        logprob, ppl = tm.logprob_and_gdns_lda(ldamdl=lda_model,counts=matrix)
+        print(logprob)
+        print(ppl)
 
     def test_predict(self):
         num_topics = 2
-        lda_model = tm.fit_lda(bag=bow_corpus, numTopics=num_topics, counts=None)
-        topicIdx = tm.predict(bag=bow_corpus, documents=None, counts=None, ldamdl=lda_model)
+        lda_model = tm.fit_lda(bag=bow_corpus, numTopics=num_topics)
+        topicIdx, score= tm.predict(bag=bow_corpus, ldamdl=lda_model)
         print(topicIdx)
+        print(score)
+
+    def test_predict_document_input(self):
+        num_topics = 2
+        lda_model = tm.fit_lda(bag=bow_corpus, numTopics=num_topics)
+        topicIdx, score= tm.predict(documents=document_fruit, ldamdl=lda_model)
+        print(topicIdx)
+        print(score)
+
+    def test_predict_matrix_input(self):
+        num_topics = 2
+        lda_model = tm.fit_lda(bag=bow_corpus, numTopics=num_topics)
+        topicIdx, score= tm.predict(counts=matrix, ldamdl=lda_model)
+        print(topicIdx)
+        print(score)
+
 
     def test_transform_to_lowdimension(self):
-        num_topics = 2
+        num_topics = 4
         lda_model = tm.fit_lda(bag=bow_corpus, numTopics=num_topics, counts=None)
-        lsa_model = tm.fit_lsa(bag=bow_corpus, numTopics=num_topics, counts=None)
 
-        dscore_lda = tm.transform_to_lowdimension(ldamdl=lda_model, documents=None, bag=bow_corpus, counts=None)
-        dscore_lsa = tm.transform_to_lowdimension(lsamdl=lsa_model, documents=None, bag=bow_corpus, counts=None)
+        dscore_lda_documents = tm.transform_to_lowdimension(ldamdl=lda_model, documents=document_fruit)
+        dscore_lda_bag = tm.transform_to_lowdimension(ldamdl=lda_model, bag=bow_corpus)
+        dscore_lda_count = tm.transform_to_lowdimension(ldamdl=lda_model, counts=matrix)
 
-        print(dscore_lda)
-        print(dscore_lsa)
+        print(dscore_lda_documents)
+        print(dscore_lda_bag)
+        print(dscore_lda_count)
+
+        lsa_model = tm.fit_lsa(bag=bow_corpus, numComponents=num_topics)
+
+        dscore_lsa_count = tm.transform_to_lowdimension(lsamdl=lsa_model, counts=matrix)
+        dscore_lsa_bag = tm.transform_to_lowdimension(lsamdl=lsa_model, bag=bow_corpus)
+        dscore_lsa_documents = tm.transform_to_lowdimension(lsamdl=lsa_model, documents=document_fruit)
+
+        print(dscore_lsa_count)
+        print(dscore_lsa_bag)
+        print(dscore_lsa_documents)
